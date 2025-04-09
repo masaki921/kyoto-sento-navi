086a3a2d51b5641ccb36787679525c4269abec7c
masaki921 <masakiogura.0921@gmail.com>
2025-04-10 03:44:12 +0900

    Delete kyoto-sento-navi/pages directory

--- kyoto-sento-navi/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>京の湯ナビ</h1>
-      <p>京都の銭湯を検索・発見するウェブサイト</p>
-    </main>
-  );
-}
